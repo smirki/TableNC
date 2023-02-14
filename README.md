f32f7fe6efd443f7632c68c46dc5ea2cbbca3dce
Manav M <manav.maj@outlook.com>
2023-02-14 10:27:20 -0500

    Added Navbar Control

--- README.md
+++ README.md
@@ -1,16 +1,38 @@
-# tablenc
 
-A new Flutter project.
+# Table NC App
 
-## Getting Started
+![Navbar](display_images/navbar_built.gif)
 
-This project is a starting point for a Flutter application.
+## Description
 
-A few resources to get you started if this is your first Flutter project:
+The Table NC App is designed to help users request food delivery or nutrition education, learn about volunteering opportunities, access nutrition curriculum and recipes, and donate to Table NC.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+## Pages and Content
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+-   Main Page: Introduction to the app and options to Request Food or Request Nutrition Education.
+-   Food Program Page: Information about the Food Program, including how it works, impact numbers, customer testimonials, what is included in each bag, and sample menus. Sign-up button to request food delivery.
+-   Nutrition Education Program Page: Information about the Nutrition Education Program, including the latest initiatives, sample nutrition ed kit, links to blog, and an option to ask a staff member for more information. Sign-up button to request nutrition education kits.
+-   Volunteer Page: Overview of volunteering opportunities at Table NC and information on how to become a volunteer. Three-step process to sign up to become a volunteer: Learn More, Complete Volunteer Registration, and Sign up for Volunteer Shifts.
+-   Eating Well Page: Access to the Nutrition Curriculum and Recipes.
+-   Donate Page: Options to donate food, funds, or other items to Table NC.
+-   Sign In Page: Option for staff, volunteers, donors, and participants to sign in to access additional content.
+
+## Usage
+
+Users can access the Table NC App and request food delivery or nutrition education, learn about volunteering opportunities, access nutrition curriculum and recipes, and donate to Table NC.
+
+## Installation
+
+The Table NC App can be downloaded from the App Store or Google Play.
+
+## Credits
+
+This app was developed by @smirki.
+
+## License
+
+This app is licensed under the Apache-2.0.
+
+## Contact
+
+For any inquiries, please contact Table NC at [insert contact email or phone number here].
--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,138 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-/// main application widget
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Table NC';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatelessWidget(),
+      title: 'TableNC',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    FoodProgramPage(),
+    NutritionEducationPage(),
+    VolunteerPage(),
+    EatingWellPage(),
+    DonatePage(),
+    SignInPage(),
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Food Program'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTapped,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood),
+            label: 'Food Program',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Nutrition Education',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Volunteer',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant_menu),
+            label: 'Eating Well',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            label: 'Donate',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Sign In',
+          ),
+        ],
       ),
     );
   }
 }
 
-/// stateless widget that the main application instantiates
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
-
+class FoodProgramPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const SizedBox(height: 30),
-        Text('Welcome to TableNC'),
-        const SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            TextButton(
-              onPressed: () {},
-              child: const Text('Yes'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('No'),
-            ),
-          ],
-        )
-      ],
+    return Center(
+      child: Text('This is the Food Program page'),
+    );
+  }
+}
+
+class NutritionEducationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('This is the Nutrition Education page'),
+    );
+  }
+}
+
+class VolunteerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('This is the Volunteer page'),
+    );
+  }
+}
+
+class EatingWellPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('This is the Eating Well page'),
+    );
+  }
+}
+
+class DonatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('This is the Donate page'),
+    );
+  }
+}
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('This is the Sign In page'),
     );
   }
 }
